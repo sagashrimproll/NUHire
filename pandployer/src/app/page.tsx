a2bd9dd513c92638a382fe1eb5a4227890ad4e43
sagashrimproll <batchelor.sa@northeastern.edu>
2025-02-03 16:52:50 -0500

    Discord on the page.

---
+++ pandployer/src/app/page.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="Homepage">
+      <main className="Options">
+
+      </main>
+      <footer>
+        <a
+          className="Discord link"
+          href="https://discord.gg/XNjg2VMR"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/discord.svg"
+            alt="Discord icon"
+            width={16}
+            height={16}
+          />
+          Discord
+        </a>
+      </footer>
+    </div>
+  );
+}
