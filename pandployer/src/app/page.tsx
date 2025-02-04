26edfd220fd6c5ea39f7fcd64fa62ef95f595eca
SaiDhanasiri <dhanasiri.s@northeastern.edu>
2025-02-04 09:37:18 -0500

    working on the home page

--- pandployer/src/app/page.tsx
+++ pandployer/src/app/page.tsx
@@ -1,11 +1,24 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="Homepage">
       <main className="Options">
-
+        <a href = "src/app/interview.tsx">
+          <button className="interview">Interview </button>
+          </a>
+          <a href = "src/app/jobdes.tsx">
+          <button className="jobdes">Job Description </button>
+          </a>
+          <a href = "src/app/offer.tsx">
+          <button className="offer">Send Offer </button>
+          </a>
+          <a href = "src/app/res-review.tsx">
+          <button className="res-review">Resume Review </button>
+          </a>
       </main>
+
       <footer>
         <a
           className="Discord link"
