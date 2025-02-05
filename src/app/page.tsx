6edac4ad05ffeaba22150df67e5f022331cf4963
sagashrimproll <batchelor.sa@northeastern.edu>
2025-02-05 10:40:02 -0500

    More design and moving home button to top.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,30 +3,38 @@ import Link from "next/link";
 import React from "react";
 
 
+
 export default function Home() {
   return (
     <div className="Homepage">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link href="https://fonts.googleapis.com/css2?family=Poiret+One&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
       <div className="Header">
-        <h1>Pandployer</h1>
+        <h1>
+          <Link href="/"  className = "home-button"> Home </Link>
+          Pandployer
+        </h1>
       </div>  
       <main className="Options">
         <div className = "Video">
-          <Image className="ATS-IMG"
-            src="/ATSCoverIMG.png"
-            alt="ATS Cover"
-            width={500}
-            height={500} />
           <a 
             className="ATS-video" 
             href="https://www.youtube.com/watch?v=fHpVPkIGVyY"
-          /> ATS Video
+          >
+            <Image className="ATS-IMG"
+              src="/ATSCoverIMG.png"
+              alt="ATS Cover"
+              width={700}
+              height={700} />
+            Click to watch a video in what ATS is.
+          </a>
   
         </div>
         <div className = "Buttons">
-          <Link href="/interview" className = "button"> Interview Page </Link>
-          <Link href="/jobdes"  className = "button"> Job Description </Link>
-          <Link href="/res-review"  className = "button"> Resume Review </Link>
-          <Link href="/"  className = "button"> Home </Link>
+          <Link href="/interview" className = "interview-button"> Interview Page </Link>
+          <Link href="/jobdes"  className = "jobdes-button"> Job Description </Link>
+          <Link href="/res-review"  className = "res-button"> Resume Review </Link>
         </div>
       </main>
 
