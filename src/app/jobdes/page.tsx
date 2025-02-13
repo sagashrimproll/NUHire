8c1ca455bb747fe5fc09b197a65057ce961bbad2
SaiDhanasiri <dhanasiri.s@northeastern.edu>
2025-02-13 14:11:24 -0500

    Remove deleted files

--- src/app/jobdes/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function JobDescriptionPage() { 
-return (
-    <div> 
-        <h1> Job description Page </h1>
-        <p> Content will be stored here </p> 
-        </div> 
-)
-}
--- src/app/res-review/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ResumesPage() {
-    return (
-      <div>
-        <h1>Resumes Page</h1>
-        <p>Content for the resumes page...</p>
-      </div>
-    );
-  }
