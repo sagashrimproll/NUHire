355d80380b31cada15172d0e5e58b41f0b4375e7
SaiDhanasiri <dhanasiri.s@northeastern.edu>
2025-03-21 17:01:04 -0400

    merged and made popups more relevant and up and front of the screen

--- api/server.js
+++ api/server.js
@@ -129,7 +129,7 @@ io.on("connection", (socket) => {
           // Example: Handle messages from clients
   socket.on("message", (data) => {
     console.log("Received message:", data);
-    
+
     // Broadcast the message to all connected clients
     io.emit("message", data);
   });
--- frontend/src/app/components/popup.tsx
+++ frontend/src/app/components/popup.tsx
@@ -9,14 +9,13 @@ interface PopupProps {
 }
 
 const Popup = ({ headline, message, onDismiss }: PopupProps) => {
-
   return (
-    <div className="fixed top-10 right-10 bg-springWater p-4 rounded-md shadow-lg">
-      <h2 className="font-bold text-navyHeader">{headline}</h2>
-      <p className="text-navy">{message}</p>
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-springWater p-8 w-96 rounded-md shadow-lg z-50">
+      <h2 className="font-bold text-navyHeader text-2xl mb-4">{headline}</h2>
+      <p className="text-navy text-lg mb-6">{message}</p>
       <button
         onClick={onDismiss}
-        className="mt-2 px-4 py-2 bg-gray-800 text-white rounded"
+        className="w-full px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-700"
       >
         Dismiss
       </button>
